4808e79a975692b63630459de788f3b48b64d2bf
William Pederzoli <wpederzoli@gmail.com>
2022-05-05 13:26:00 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <thread>
+#include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+  unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+  const char* src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if(result == GL_FALSE)
+  {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char* message = (char*)alloca(length * sizeof(char) );
+    glGetShaderInfoLog(id, length, &length, message);
+
+    std::cout << "Failed to compile " << 
+      (GL_FRAGMENT_SHADER == type ? "vertex " : "fragment ") << 
+      "shader!\n" << message << std::endl;
+    
+    glDeleteShader(id);
+    
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+  unsigned int program =  glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_VERTEX_SHADER, fragmentShader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
 
 int main()
 {
@@ -20,10 +65,15 @@ int main()
   glewExperimental = GL_TRUE;
   glewInit();
 
-  GLuint vertexBuffer;
-  glGenBuffers(1, &vertexBuffer);
-
-  printf("%u\n", vertexBuffer);
+  const char* vertexShader = 
+    R"
+      #version 330 core
+      
+      void main()
+      {
+        
+      }
+    ";
 
   while(!glfwWindowShouldClose(window) )
   {
